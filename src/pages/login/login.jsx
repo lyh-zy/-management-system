ab4fd3824442252e69d6d8201349a6b0438bc581
Your <LiuYuhang09513@gmail.com>
2019-07-14 22:46:30 +0800

    message

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import "./login.less"
+import logo from "./images/logo.png";
 export default class Login extends Component {
     render() {
         return (
             <div className="login">
                 <div className="login-header">
-                    <img src="" alt=""/>
-                    <h1></h1>
+                    <img src={logo} alt="logo"/>
+                    <h1>后台管理系统</h1>
+                </div>
+                <div className="login-content">
+                    <h1>用户登录</h1>
+                    <div>From组件页面</div>
                 </div>
             </div>
         )
