3f65688d3c0b4fa91c0db35ec2c332af9f94166d
Your <LiuYuhang09513@gmail.com>
2019-07-15 20:08:07 +0800

    message

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,20 +1,52 @@
 import React, { Component } from 'react'
-// import {Form,Icon,Input,Button,message} from 'antd'
+import { Form, Icon, Input, Button} from 'antd'
 import logo from './images/logo.png';
 import './login.less';
+
 export default class Login extends Component {
+    handleSubmit = e => {
+        //阻止事件默认行为,阻止表单的提交
+        e.preventDefault()
+        alert('发送登录的ajax请求')
+     
+    }
     render() {
         return (
             <div className="login">
                 <div className="login-header">
-                    <img src={logo} alt="logo"/>
+                    <img src={logo} alt="logo" />
                     <h1>后台管理系统</h1>
                 </div>
                 <div className="login-content">
                     <h1>用户登录</h1>
-                    <div>From组件页面</div>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Form.Item>
+                            <Input
+                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                placeholder="Username"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Input
+                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                type="password"
+                                placeholder="Password"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">登录</Button>
+                        </Form.Item>
+                    </Form>                 
                 </div>
             </div>
         )
     }
 }
+
+/*
+用户名/密码的的合法性要求
+  1). 必须输入
+  2). 必须大于等于4位
+  3). 必须小于等于12位
+  4). 必须是英文、数字或下划线组成
+ */
