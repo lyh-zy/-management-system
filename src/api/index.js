db87180a46963543b83396be5f6c4e9190599683
Your <LiuYuhang09513@gmail.com>
2019-07-18 11:41:10 +0800

    message

--- src/api/index.js
+++ src/api/index.js
@@ -31,3 +31,32 @@ export const reqWeather = (city) => {
   })
 
 }
+
+// const persons/personList/personArr = [{}, {}]
+
+// 获取分类列表
+// export const reqCategorys = () => ajax.get(BASE + '/manage/category/list')
+/* export const reqCategorys = () => ajax({
+  // method: 'GET',
+  url: BASE + '/manage/category/list'
+}) */
+export const reqCategorys = () => ajax(BASE + '/manage/category/list')
+
+// 添加分类
+export const reqAddCategory = (categoryName) => ajax.post(BASE + '/manage/category/add', {
+  categoryName
+})
+
+// 修改分类
+export const reqUpdateCategory = ({ categoryId, categoryName }) => ajax.post(BASE + '/manage/category/update', {
+  categoryId,
+  categoryName
+})
+
+/* 获取商品分页列表 */
+export const reqProducts = (pageNum, pageSize) => ajax(BASE + '/manage/product/list', {
+  params: { // 包含所有query参数的对象
+    pageNum,
+    pageSize
+  }
+})
